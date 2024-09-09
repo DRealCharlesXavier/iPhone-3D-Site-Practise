98b34bd8d943f9d8883e7f14ef00f3ff0e9942ee
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-09-08 14:58:57 -1100

    Practise

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  timeout,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  DiamondPlugin,
+  FrameFadePlugin,
+  GLTFAnimationPlugin,
+  GroundPlugin,
+  BloomPlugin,
+  TemporalAAPlugin,
+  AnisotropyPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  ITexture,
+  TweakpaneUiPlugin,
+  AssetManagerBasicPopupPlugin,
+  CanvasSnipperPlugin,
+  IViewerPlugin,
+  FileTransferPlugin,
+
+  // Color, // Import THREE.js internals
+  // Texture, // Import THREE.js internals
+} from "webgi";
+
+function WebgiViewer() {
+  const canvasRef = useRef(null);
+
+  return (
+    <div id="webgi-canvas-container">
+      <canvas id="webgi-canvas" ref={canvasRef} />
+    </div>
+  );
+}
+
+export default WebgiViewer;
